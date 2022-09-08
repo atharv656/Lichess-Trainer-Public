bca24c07adf6d04861c38504a737be63c5c4e889
atharv656 <atharv.v.agashe.25@dartmouth.edu>
2022-09-07 21:57:00 -0400

    Update Log Output

--- dailyRatingScript.py
+++ dailyRatingScript.py
@@ -6,7 +6,7 @@ session = berserk.TokenSession("lip_DcekuZBitKMSNtho2eJN")
 client = berserk.Client(session=session)
 today = date.today().strftime("%m/%d/%y")
 rapid = client.users.get_by_id("redchess656")[0]['perfs']['rapid']
-# writer.writerow([today, rapid['games'], rapid['rating']])
+
 with open('dailyLog.csv', 'a') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["ahhh", "WTF", "BITCHHHH"])
+    writer.writerow([today, rapid['games'], rapid['rating']])
